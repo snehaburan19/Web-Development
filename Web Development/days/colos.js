4d431c92bcd1f4fb1090361429c3e800e143c604
B Sneha <108780123+snehaburan19@users.noreply.github.com>
2025-04-12 16:37:13 +0530

    Add files via upload

---
+++ Web Development/days/colos.js
@@ -0,0 +1,56 @@
+//1.eligible for vote or not using else-if 
+var age=27;
+if(age>0 && age<=11){
+    console.log("not eligible for vote");
+}else if(age>11 && age<=17){
+    console.log("not eligibe for vote");
+}else{
+    console.log("eligible for vote");
+}
+//2.print numbers from 1-10 using for loop
+for(var i=1;i<=10;i++){
+  console.log(i);
+}
+//3.print numbers form 25-50 using for loop
+for(var i=25;i<=50;i++){
+    console.log(i);
+}
+//4.print numbers from 20-5 using for loop
+for(var i=20;i>=5;i--){
+    console.log(i);
+}
+//5.print numbers as 1,3,5,7,9,11 using while loop
+var i=1;
+while(i<=11){
+    console.log(i);
+    i+=2;
+}
+/* or using to check with % operator i%2==1
+while((i%2==1)&&(i<=11)){
+    console.log(i);
+    i+=2;
+}*/
+//6.print numbers from 12-1 using whie loop
+var i=12;
+while(i>=1){
+    console.log(i);
+    i--;
+}
+//7.print numbers from 1-10 uding while loop
+var i=1;
+while(i<=10){
+    consol.log(i);
+    i++;
+}
+//8.print numbers from 2-12 using while loop
+var i=2;
+while(i<=12){
+    console.log(i);
+    i++;
+}
+//9.print numbers from 32-44 using while loop
+var i=32;
+while(i<=44){
+    console.log(i);
+    i++;
+}
