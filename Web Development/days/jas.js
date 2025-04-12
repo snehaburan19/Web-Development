9be35e8d2200ac469d8ed32e51243639635ed784
B Sneha <108780123+snehaburan19@users.noreply.github.com>
2025-04-12 15:24:56 +0530

    Add files via upload

---
+++ Web Development/days/jas.js
@@ -0,0 +1,102 @@
+console.log("Sneha");
+console.log("Buran");
+console.log("WebDev");
+
+var a=3;
+var b=4;
+
+//Arithmetic Op
+
+console.log('Addition: ${a+b');
+console.log('Substraction: ${a-b}');
+console.log('Multiplication: ${a*b}');
+console.log('Exponential: ${a**b}');
+console.log('Division: ${a/b}');
+console.log('Modulo: ${a%b}');
+
+//Assignment Op
+
+let x=3;
+x+=6;
+console.log('+=: ${x}');
+
+let y=4;
+y-=6;
+console.log('-=: ${y}');
+
+let z=5;
+z*=6;
+console.log('*=: ${z}');
+
+let x1=4;
+x1%=6;
+console.log('%=: ${x1}');
+
+let y1=4;
+y1**=6;
+console.log('**=: ${y1}');
+
+let z1=4;
+z1/=6;
+console.log('/=: ${z1}');
+
+//Comparison Op
+
+console.log('Equal to ${a == b}');
+console.log('Not equal to ${a != b}');
+console.log('Greator than ${a > b}');
+console.log('Less than ${a < b}');
+console.log('Greator than or equal to ${a >= b}');
+console.log('Less than ${a <= b}');
+
+//Logical Op
+
+let A = true;
+let B = false;
+
+console.log('AND ${A && B'); //F
+console.log('AND ${A && B'); //T
+console.log('OR ${!A || B'); //F
+console.log('OR ${A || B'); //T
+console.log('NOT ${!A}'); //F
+console.log('NOT ${!B}'); //T
+
+//Bitwise Op
+
+console.log('AND ${a & b}');
+console.log('OR ${a | b}');
+console.log('XOR ${a ^ b}');
+console.log('NOT ${~a}');
+console.log('LEFT SHIFT ${a << 1}');
+console.log('RIGHT SHIFT ${a >> 1');
+
+//Conditional Op
+
+let year = 2000;
+let result = (year >= 2000) ? 'GEN Z': 'Milleneals';
+comsole.log(firstname +" "+ lastname); //concatination
+
+//Unary Op
+
+let s=10;
+console.log('Pre=increment ${++s}');
+console.log('Post-increment ${s++}');
+console.log('Pre-decrement ${--s}');
+console.log('Post-decrement ${s--}');
+console.log('typeof ${typeof s}');
+console.log('Unary plus ${+s}');
+
+//Type Op
+let u = 10;
+let v = "Hello";
+console.log(typeof p);
+console.log(typeof q);
+console.log(typeof null); //object
+console.log(typeof undefined);
+
+//Comma Op
+
+let m = 1;
+let n = 2;
+let res =(a++, b++)
+console.log(a, b);
