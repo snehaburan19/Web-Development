fbfee81a7671c330340feb59d99c404dc7c9db01
B Sneha <108780123+snehaburan19@users.noreply.github.com>
2025-04-28 22:35:28 +0530

    Add files via upload

---
+++ Web Development/days/day24.js
@@ -0,0 +1,10 @@
+//step 1:Selecting an element
+var h1=document.querySelector('h1');
+console.log(h1);
+//or
+h1.addEventListener('click',function(){
+h1.innerHTML='Hi,Good Afternoon Everyone';
+h1.innerHTML="crimson";
+h1.style.backgroundColor='black';
+h1,style.fontSize='16px';
+})
